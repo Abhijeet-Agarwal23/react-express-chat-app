a9e0d0c6d42a29bb5e1a1f6337d6d1ca7cb97854
Abhijeet Agarwal <abhijeet.agarwal23@gmail.com>
2021-04-22 21:08:42 +0530

    Add basic server setup

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
 const path = require('path');
+const router = require('./router');
+
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -7,15 +11,23 @@ const DIST_DIR = path.join(__dirname, '../dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
 app.use(express.static(DIST_DIR));
-
-app.get('/api', (req, res) => {
-  res.send('Hello from express api');
-});
+app.use('/api', router);
 
 app.get('*', (req, res) => {
   res.sendFile(HTML_FILE);
 });
 
-app.listen(port, function () {
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connect', (socket) => {
+  console.log('we have a new connection');
+
+  socket.on('disconnect', () => {
+    console.log('User has left');
+  });
+});
+
+server.listen(port, () => {
   console.log('App listening on port: ' + port);
 });
